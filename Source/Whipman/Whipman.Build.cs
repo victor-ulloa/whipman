b5fb1dfa1980b289f2eb588e307d1903197255a0
Alejandro Ulloa <alejandroekas@gmail.com>
2023-02-21 17:06:54 -0500

    slate enabled

--- Source/Whipman/Whipman.Build.cs
+++ Source/Whipman/Whipman.Build.cs
@@ -9,11 +9,8 @@ public class Whipman : ModuleRules
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
-
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
-
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+		
+		PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 		
 		// Uncomment if you are using online features
 		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
